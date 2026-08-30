977601944429033a85c5a72aa3d8974f95209bb5
Irina Mishina <irinamishina131@gmail.com>
2026-08-30 03:38:14 -0400

    music added

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
